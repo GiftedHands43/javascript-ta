716e2875f63b3ba3326f37c1d3566fe78ffdbcf0
GiftedHands43 <100702359+GiftedHands43@users.noreply.github.com>
2022-06-14 13:46:21 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 // complete the function
-function prime (num) {
+function prime (number) {
   // code goes here
+  const number = parseInt(prompt("Enter a positive number: "));
+  let isPrime = true;
+
+// check if number is equal to 1
+if (number === 1) {
+    console.log("1 is not a prime number.");
+}
+
+// check if number is greater than 1
+else if (number > 1) {
+
+    for (let i = 2; i < number; i++) {
+        if (number % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime) {
+        console.log(`${number} is a prime number`);
+    } else {
+        console.log(`${number} is a not prime number`);
+    }
+}
+
+// check if number is less than 1
+else {
+    console.log("The number is not a prime number.");
+}
+}
 }
 
 function solution (arg) {
